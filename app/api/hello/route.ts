ddd82ada8feaa9918b1a30c9f63cdd61993bff61
lumen-limitless <lumenlimitless@gmail.com>
2025-03-22 19:20:39 -0400

    remove example route

--- app/api/hello/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from "next/server"
-
-export async function GET(req: NextRequest) {
-  console.log(req)
-  return NextResponse.json({
-    message: "Hello, World!",
-  })
-}
