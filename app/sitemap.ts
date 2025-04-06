86b151ee963c08bf0be6d16ec9bedbcae4896b70
lumen-limitless <lumenlimitless@gmail.com>
2025-04-06 10:31:17 -0400

    fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,10 +2,10 @@ import { type MetadataRoute } from "next"
 
 const baseUrl = new URL(
   process.env.NEXT_PUBLIC_APP_URL
-    ? `https://${process.env.NEXT_PUBLIC_APP_URL}`
+    ? `${process.env.NEXT_PUBLIC_APP_URL}`
     : process.env.NEXT_PUBLIC_VERCEL_URL
-      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-      : "http://localhost:3000"
+      ? `${process.env.NEXT_PUBLIC_VERCEL_URL}`
+      : "localhost:3000"
 )
 
 export default function Sitemap(): MetadataRoute.Sitemap {
