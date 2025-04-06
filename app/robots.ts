4ca265652645dc862efc7381fcd4a63d85aac45b
lumen-limitless <lumenlimitless@gmail.com>
2025-04-06 10:37:58 -0400

    robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,9 +1,9 @@
 import { type MetadataRoute } from "next"
 
 const baseUrl = process.env.NEXT_PUBLIC_APP_URL
-  ? `https://${process.env.NEXT_PUBLIC_APP_URL}`
+  ? `${process.env.NEXT_PUBLIC_APP_URL}`
   : process.env.NEXT_PUBLIC_VERCEL_URL
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    ? `${process.env.NEXT_PUBLIC_VERCEL_URL}`
     : "http://localhost:3000"
 
 export default function robots(): MetadataRoute.Robots {
